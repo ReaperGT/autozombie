06801695a135eede753c760d0a104e133414c296
anjgarong <anj@klab.com>
2014-01-04 06:51:44 +0800

    - folder structure under Auto Zombie Folders - in-game tasks: tried using own map (see the OwnSample scene)

---
+++ Auto Zombie/Assets/Auto Zombie Folders/Scripts/HumanCarAnimation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanCarAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			animation.CrossFade("turning R");
+			//Invoke("ResumeMovingAnimation", animation["turning R"].length);
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			animation.CrossFade("turning L");
+			//Invoke("ResumeMovingAnimation", animation["turning L"].length);
+		}
+
+		if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			ResumeMovingAnimation();
+		}
+	}
+
+	void ResumeMovingAnimation()
+	{
+		animation.CrossFade("moving");
+	}
+}
