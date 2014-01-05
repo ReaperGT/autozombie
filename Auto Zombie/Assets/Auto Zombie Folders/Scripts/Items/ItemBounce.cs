112c03878c39cc6d56e5bcb8588342cb14878d27
Cris Uy <alvin_uy140@yahoo.com.ph>
2014-01-05 22:52:56 +0800

    Add Vaccine and Nitro in map (including destroy when hit by player)

---
+++ Auto Zombie/Assets/Auto Zombie Folders/Scripts/Items/ItemBounce.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemBounce : MonoBehaviour {
+	public float distanceY, speed, delay;
+	
+	private float startingYpos, maxYpos;
+	private bool isGoingUp = true, okToMove = true;
+	
+	// Use this for initialization
+	void Start () {
+		startingYpos = transform.position.y;
+		maxYpos = startingYpos + distanceY;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (okToMove)
+			StartMoveUpDown ();
+	}
+	
+	void StartMoveUpDown () {
+		if (isGoingUp) {
+			transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+			
+			if (transform.position.y > maxYpos) {
+				isGoingUp = false;
+			}
+		} else {
+			transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+			
+			if (transform.position.y < startingYpos) {
+				isGoingUp = true;
+				okToMove = false;
+				Invoke ("RestartMoving", delay);
+			}
+		}
+	}
+	
+	void RestartMoving() {
+		okToMove = true;
+	}
+
+}
---
+++ Auto Zombie/Assets/Auto Zombie Folders/Scripts/Items/ItemDestroy.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PowerUp {
+	PowerUpBoost	= 0,
+	PowerUpNitro	= 1,
+	PowerUpVaccine 	= 2,
+};
+
+public class ItemDestroy : MonoBehaviour {
+	
+	public PowerUp powerUp;
+	
+	void OnTriggerEnter(Collider collider) {
+		Debug.Log("Enter!");
+		switch (powerUp) {
+		case PowerUp.PowerUpBoost:
+			// Add - Boost
+			break;
+		case PowerUp.PowerUpNitro:
+			// Add - Nitro
+			break;
+		case PowerUp.PowerUpVaccine:
+			// Add - Vaccine
+			break;
+		default:
+			break;
+		}
+		
+		if (collider.CompareTag("CarChasisCollider")) {
+			Destroy(this.gameObject);
+		}
+	}
+	
+	void updateUIEffectOnScreen() {
+		// Add Effect On Screen
+	}
+	
+	void updateUIEffectOnCar() {
+		// Add Effect On Car
+		// not sure kung d2 sa script gagawin yung change when collided
+	}
+}
