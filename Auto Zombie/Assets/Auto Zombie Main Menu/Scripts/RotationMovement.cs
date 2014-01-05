018bc1dab00905626500190441d823dd3ca4104e
karencatearabit <karen@klab.com>
2014-01-05 21:08:34 +0800

    added main menu

---
+++ Auto Zombie/Assets/Auto Zombie Main Menu/Scripts/RotationMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationMovement : MonoBehaviour {
+	
+	[HideInInspector]public float rotationValue = 180;
+	
+	void Update () {
+		transform.localRotation = Quaternion.Euler(0f, rotationValue, 0f); 
+		rotationValue -= 0.25f; 
+	}
+}
+
+
+
