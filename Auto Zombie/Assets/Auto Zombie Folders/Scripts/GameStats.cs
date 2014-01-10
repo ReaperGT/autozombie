e996c6224b982b399aab72bec9eba1b6410eb977
karencatearabit <karen@klab.com>
2014-01-10 13:06:53 +0800

    modified gamestats

--- Auto Zombie/Assets/Auto Zombie Folders/Scripts/GameStats.cs
+++ Auto Zombie/Assets/Auto Zombie Folders/Scripts/GameStats.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿﻿using UnityEngine;
 using System.Collections;
 
 public static class GameStats {
@@ -6,4 +6,19 @@ public static class GameStats {
 	public static int AZ_selectedCar;
 	public static int AZ_selectedCharacter;
 	public static bool AZ_OnVillage;
+
+	public static float bestTime = 0f;
+
+	public static void saveBestTime(float raceTime)
+	{
+		if (raceTime > bestTime)
+		{
+			PlayerPrefs.SetFloat("BestTime", raceTime);
+		}
+	}
+
+	public static float getBestTime()
+	{
+		return PlayerPrefs.GetFloat("BestTime", 0f);
+	}
 }
